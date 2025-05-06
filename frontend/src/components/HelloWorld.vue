fdb0285d068417cb876b8c3f84509ddfabe35eba
KhaledAltahrawi <kaltahra@my.centennialcollege.ca>
2025-05-05 21:29:19 -0400

    Added frontend directory as a regular directory

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Barber Appointment Booking</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #333;
+  text-align: center;
+}
+</style>
